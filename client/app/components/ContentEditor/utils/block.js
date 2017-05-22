9a6f3df5894712882db8300aa1c3d94e8d0fac33
Bryan Haakman <bryanhaakman@gmail.com>
2017-05-22 11:22:22 +0200

    add back in insertDataBlock

--- client/app/components/ContentEditor/utils/block.js
+++ client/app/components/ContentEditor/utils/block.js
@@ -5,6 +5,8 @@ import {
     RichUtils,
     Modifier,
     ContentBlock,
+    BlockMapBuilder,
+    genKey,
 } from 'draft-js';
 import {
     OrderedMap,
@@ -196,3 +198,60 @@ export function getSelectedBlocksMetadata(editorState: EditorState): Map {
     }
     return metaData;
 }
+
+export function insertDataBlock(editorState, data) {
+    const contentState = editorState.getCurrentContent();
+    const selectionState = editorState.getSelection();
+
+    const afterRemoval = Modifier.removeRange(
+        contentState,
+        selectionState,
+        "backward"
+    );
+
+    const targetSelection = afterRemoval.getSelectionAfter();
+    const afterSplit = Modifier.splitBlock(afterRemoval, targetSelection);
+    const insertionTarget = afterSplit.getSelectionAfter();
+
+    const asAtomicBlock = Modifier.setBlockType(
+        afterSplit,
+        insertionTarget,
+        "atomic"
+    );
+
+    const block = new ContentBlock({
+        key: genKey(),
+        type: "atomic",
+        text: "",
+        characterList: List(),
+        data: new Map(data)
+    });
+
+
+    const fragmentArray = [
+        block,
+        new ContentBlock({
+            key: genKey(),
+            type: "unstyled",
+            text: "",
+            characterList: List()
+        })
+    ];
+
+    const fragment = BlockMapBuilder.createFromArray(fragmentArray);
+
+    const withAtomicBlock = Modifier.replaceWithFragment(
+        asAtomicBlock,
+        insertionTarget,
+        fragment
+    );
+
+    const newContent = withAtomicBlock.merge({
+        selectionBefore: selectionState,
+        selectionAfter: withAtomicBlock.getSelectionAfter().set("hasFocus", true)
+    });
+
+    return EditorState.push(editorState, newContent, "insert-fragment");
+}
+
+
