8fe35be504e5ae5d31f75da7ad2567c9d9cf53fb
Bryan Haakman <bryanhaakman@gmail.com>
2017-05-22 11:36:47 +0200

    fix routing in auth

--- client/auth/app.js
+++ client/auth/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom"
+import withProps from 'recompose/withProps'
 
 import Forgot, { ForgotSuccess } from './Forgot'
 import Reset, { ResetSuccess } from './Reset'
@@ -9,12 +10,6 @@ import Signup, { SignupSocial, SignupSuccess, SignupVerificationSuccess } from '
 import EmailConfirmation, { EmailConfirmationSuccess } from './EmailConfirmation'
 import AccountVerification, { VerificationResendSuccess } from './Verification'
 
-// TODO: just require foundaiton in some way... - 2016-08-09
-// const jQuery = require('jquery')
-// const $ = jQuery
-// const foundation = require('foundation-sites')
-
-// $(document).foundation();
 import "../scss/landing.scss"
 
 // TODO: get config in context somehow - 2016-08-11
@@ -25,54 +20,43 @@ const App = (props) => (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div className="row stretch">
             <div className="main-container stretch columns small-centered small-12 medium-9 large-5 xlarge-4 xxlarge-3">
-                <Switch>
-                    <Route path="login" component={Login}/>
-                    <Route path="signup" component={Signup}/>
-                    <Route path="signup/success" component={SignupVerificationSuccess}/>
-                    <Route path="signup/social" component={EmailConfirmation}/>
-                    <Route path="signup/social/:email" component={EmailConfirmation}/>
-                    <Route path="signup/social/success" component={EmailConfirmationSuccess}/>
-                    <Route path="login/forgot" component={Forgot}/>
-                    <Route path="login/forgot/success" component={ForgotSuccess}/>
-
-                    <Route path="login/reset/:email/:token" component={Reset}/>
-                    <Route path="login/reset/success" component={ResetSuccess}/>
-
-                    <Route path="account/verification" component={AccountVerification}/>
-                    <Route path="account/verification/success" component={VerificationResendSuccess}/>
-                    <Route path="*" component={Login}/>
-                </Switch>
+                <Router {...props} />
             </div>
         </div>
     </MuiThemeProvider>
 )
 
 // TODO: Take from the main config 2016-05-04
-const baseURL = '/auth'
+// const baseURL = '/auth'
 
-const Router = () => (
-    <BrowserRouter>
-        <Route path={baseURL} component={App} />
-    </BrowserRouter>
-)
+const Router = (props) => {
+    const withInitialState = withProps(props)
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/auth/login" component={withInitialState(Login)}/>
+                <Route exact path="/auth/signup" component={withInitialState(Signup)}/>
+                <Route exact path="/auth/signup/success" component={withInitialState(SignupVerificationSuccess)}/>
+                <Route exact path="/auth/signup/social" component={withInitialState(EmailConfirmation)}/>
+                <Route exact path="/auth/signup/social/:email" component={withInitialState(EmailConfirmation)}/>
+                <Route exact path="/auth/signup/social/success" component={withInitialState(EmailConfirmationSuccess)}/>
+                <Route exact path="/auth/login/forgot" component={withInitialState(Forgot)}/>
+                <Route exact path="/auth/login/forgot/success" component={withInitialState(ForgotSuccess)}/>
 
-function createElement(Component, props) {
-    /*
-     * Pass server rendered data as props to all components
-    */
+                <Route exact path="/auth/login/reset/:email/:token" component={withInitialState(Reset)}/>
+                <Route exact path="/auth/login/reset/success" component={withInitialState(ResetSuccess)}/>
 
-    let initialState = window.INITIAL_STATE || {}
-
-    return <Component {...props} {...initialState} />
+                <Route exact path="/auth/account/verification" component={withInitialState(AccountVerification)}/>
+                <Route exact path="/auth/account/verification/success" component={withInitialState(VerificationResendSuccess)}/>
+                <Route path="*" component={withInitialState(Login)}/>
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-     ReactDom.render(
-         <Router 
-             history={browserHistory} 
-             createElement={createElement}
-             routes={routes} 
-         />,
-          document.getElementById('app')
-     );
+    ReactDom.render(
+        <App {...(window.INITIAL_STATE || {})}/>,
+        document.getElementById('app')
+    );
 });
