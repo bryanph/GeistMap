c469bdb04ba974c55d3904a54859931a1a68f8a2
Bryan Haakman <bryanhaakman@gmail.com>
2017-11-29 13:17:36 +0100

    remove unused material-ui code

--- client/app/components/Spinner/index.js
+++ client/app/components/Spinner/index.js
@@ -5,42 +5,13 @@
  */
 
 import React from 'react';
-import CircularProgress from 'material-ui/CircularProgress';
-
-import { accentColor } from '../../containers/App/muitheme'
 
 import { Dimmer, Loader } from 'semantic-ui-react'
-
-const styles = {
-    spinner: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    },
-    inlineSpinner: {
-        display: 'inline',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // width: '100%',
-        // height: '100%',
-    },
-}
 
 export function Spinner({ size, style={} }) {
     return (
         <Loader active={true}/>
     )
-}
-
-export function InlineSpinner({ size, style={} }) {
-    const myStyles = Object.assign({}, styles.inlineSpinner, style)
-    return (
-        <div style={myStyles}>
-            <CircularProgress color={accentColor} size={size || 1.5} />
-        </div>
-    );
 }
 
 export default Spinner
