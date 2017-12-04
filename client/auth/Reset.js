883a37cb7a49f90a4c46e7a8ee0369d2c98c0480
Bryan Haakman <bryanhaakman@gmail.com>
2017-12-04 14:24:53 +0100

    fix bug with reset password, update for react router v4

--- client/auth/Reset.js
+++ client/auth/Reset.js
@@ -11,110 +11,105 @@ import createClass from 'create-react-class'
 
 const Reset = createClass({
 
-  handleResponse: function(json, response) {
-      if (Object.keys(json.errfor).length) {
-          return this.setState({validationErrors: json.errfor})
-      }
-      if (json.errors.length) {
-          return this.setState({errors: json.errors})
-      }
-
-      this.props.history.push('/auth/login/reset/success')
-  },
-
-  handleError: function(error) {
-    console.error(error)
-  },
-
-  getInitialState: function() {
-      return {
-          errors: [],
-          validationErrors: {},
-      }
-  },
-
-  render: function() {
-    const {
-        params: {
-            email,
-            token,
+    handleResponse: function(json, response) {
+        if (Object.keys(json.errfor).length) {
+            return this.setState({validationErrors: json.errfor})
         }
-    } = this.props
+        if (json.errors.length) {
+            return this.setState({errors: json.errors})
+        }
 
+        this.props.history.push('/auth/login/reset/success')
+    },
 
-    const {
-        errors,
-        validationErrors,
-    } = this.state
-    
-    return (
-        <div>
-          <ResetForm 
-            email={email}
-            token={token}
-            handleError={this.handleError}
-            handleResponse={this.handleResponse}
-          />
+    handleError: function(error) {
+        console.error(error)
+    },
 
-          { validationErrors ? <ValidationErrors errors={validationErrors} /> : null }
-          { errors ? <RenderErrors errors={errors} /> : null }
+    getInitialState: function() {
+        return {
+            errors: [],
+            validationErrors: {},
+        }
+    },
+
+    render: function() {
+        const email = this.props.match.params.email
+        const token = this.props.match.params.token
+
+        const {
+            errors,
+            validationErrors,
+        } = this.state
+
+        return (
+            <div>
+                <ResetForm 
+                    email={email}
+                    token={token}
+                    handleError={this.handleError}
+                    handleResponse={this.handleResponse}
+                />
+
+            { validationErrors ? <ValidationErrors errors={validationErrors} /> : null }
+            { errors ? <RenderErrors errors={errors} /> : null }
         </div>
-      
-    )
-  }
+
+        )
+    }
 })
 
 export default withRouter(Reset)
 
 export const ResetForm = createClass({
 
-  handleSubmit: function(e) {
-      e.preventDefault()
+    handleSubmit: function(e) {
+        e.preventDefault()
 
-      const { email, token } = this.props
+        const { email, token } = this.props
 
-      const path = `/auth/login/reset/${email}/${token}`
+        const path = `/auth/login/reset/${email}/${token}`
 
-      fetchJSON(path, {
-          method: 'PUT',
-          headers: getHeaders(),
-          body: JSON.stringify({
-              password: this._password.value,
-              confirm: this._confirm.value,
-          })
-      })
-      .then(this.props.handleResponse)
-      .catch(this.props.handleError)
-  },
+        fetchJSON(path, {
+            method: 'PUT',
+            headers: getHeaders(),
+            body: JSON.stringify({
+                password: this._password.value,
+                confirm: this._confirm.value,
+            })
+        })
+            .then(this.props.handleResponse)
+            .catch(this.props.handleError)
+    },
 
 
-  render: function() {
-    return (
-      <div className="interact panel with-logo">
-        <div className="logo"></div>
-        <h3>Reset your password</h3>
-        <p>Enter a new password in the fields below.</p>
-        <form id="signup-form" ref={c => this._form = c}>
-            <input 
-                type="password" 
-                ref={c => this._password = c}
-                placeholder="Password"
-            />
-            <input 
-                type="password" 
-                ref={c => this._confirm = c}
-                placeholder="Confirm password"
-            />
-          <button className="button input-height" onClick={this.handleSubmit}>Confirm Reset</button>
-        </form>
-      </div>
-    )
-  }
+    render: function() {
+        return (
+            <div className="interact panel with-logo">
+                <div className="logo"></div>
+                <h3>Reset your password</h3>
+                <p>Enter a new password in the fields below.</p>
+                <form id="signup-form" ref={c => this._form = c}>
+                    <input 
+                        type="password" 
+                        ref={c => this._password = c}
+                        placeholder="Password"
+                    />
+                    <input 
+                        type="password" 
+                        ref={c => this._confirm = c}
+                        placeholder="Confirm password"
+                    />
+                    <button className="button input-height" onClick={this.handleSubmit}>Confirm Reset</button>
+                </form>
+            </div>
+        )
+    }
 })
 
 export const ResetSuccess = (props) => (
-  <div>
-    <h2>Your password has succesfully been reset.</h2>
-    <Link to='/auth/login'>Back to login</Link>
-  </div>
+    <div>
+        <h2>Your password has succesfully been reset.</h2>
+        <Link to='/auth/login'>Back to login</Link>
+    </div>
 )
