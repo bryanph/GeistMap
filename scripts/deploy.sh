6063cebaeb08cc8f4f4cb401f7a0b4e113754fc6
Bryan Haakman <bryanhaakman@gmail.com>
2018-01-16 12:51:09 +0100

    change deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,16 @@
 set -e
 
 npm run build
+ssh graphtodo "mkdir -p graphtodo/tmp/public"
 ssh graphtodo "cd graphtodo && git pull"
 ssh graphtodo "cd graphtodo && npm install && npm prune"
+scp stats.json graphtodo:graphtodo/tmp
+scp -r public/*.bundle.js public/*.map public/fonts public/images graphtodo:graphtodo/tmp/public
 ssh graphtodo "cd graphtodo && npm run clean"
-scp stats.json graphtodo:graphtodo
-scp -r public/*.bundle.js public/*.map public/fonts public/images graphtodo:graphtodo/public
+ssh graphtodo "cp -r graphtodo/tmp/* graphtodo/"
 ssh graphtodo "cd graphtodo && npm link full-auth-middleware"
 ssh graphtodo "sudo systemctl restart geist.service"
+ssh graphtodo "rm -rf graphtodo/tmp"
 echo Done!
 
 
